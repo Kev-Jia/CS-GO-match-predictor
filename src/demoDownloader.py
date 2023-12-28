e95f1540af81c7cd03a42b0b31283cfbc4daa905
Kevin Jia <kevinhengkaijia@gmail.com>
2023-12-28 20:05:52 +0000

    download urls generated

--- src/demoDownloader.py
+++ src/demoDownloader.py
@@ -29,44 +29,66 @@ totalMaps = 0
 # populate lists
 for i in range(len(data)):
     matchIDs.append(data[i]["id"])
-    if data[i]["format"] != "bo1":
+
+    # filter for Bo3 matches only
+    if data[i]["format"] == "bo3":
         results.append([data[i]["result"]["team1"], data[i]["result"]["team2"]])
-    else:
-        if max(data[i]["result"], key = data[i]["result"].get) == "team1":
-            results.append([1, 0])
-        else:
-            results.append([0, 1])
 
 # total maps across 2022 Liquipedia S-tier tournaments
 totalMaps = sum(np.array(results).sum(1))
-print(totalMaps)
+print("total maps for all Bo3's 2022:", totalMaps)
 
 # getting an estimate for data size
-# IEM Katowice 2022 grand final has multiple overtimes, 3:0 Bo5 comes out to 716 MB
-# IEM Cologne 2022 grand final has fewer overtimes, 3:2 Bo5 comes out to 980 MB
-# 750 MB per match is a good rough upper estimate of mean match file size
-print("mean maps per match: ", "{:,.3f}".(totalMaps / len(data)))
-print("total maps to download: ", math.ceil(n * (totalMaps / len(data))))
-print("total GB to download: ", (math.ceil(n * (totalMaps / len(data))) * 0.75))
-print("download time: ", math.floor(math.ceil(n * (totalMaps / len(data))) * 0.75 / 0.01 / 60 / 60))
+# IEM Katowice 2022 grand final has multiple overtimes, 3:0 Bo5 comes out to ~700 MiB
+# uncompressed it comes out to ~1.5 GiB
+# 500 MiB is a good rough estimate for mean compressed match size
+# 1 GiB is a good rough estimate for mean uncompressed match size
+print("mean maps per match:", str("{:.3f}".format(totalMaps / len(data))))
+print("expected total maps to download:", math.ceil(n * (totalMaps / len(data))))
+print("download (compressed):", (math.ceil(n * (totalMaps / len(data))) * 0.5), "GiB")
+print("download (uncompressed)/dataset size:", 2 * (math.ceil(n * (totalMaps / len(data))) * 0.5), "GiB")
+
+time = (math.ceil(n * (totalMaps / len(data))) * 0.5 / 0.01 / 60 / 60)
+print("download time:", int(math.modf(time)[1]), "h,", math.ceil(math.modf(time)[0] * 60), "min\n")
 
 matchIDsToFetch = sample(matchIDs, n)
 
-matchURL = ""
+matchURLToFetch = ""
 params = []
 downloadURLs = []
 
+# fetching download URLs for all the match IDs randomly sampled
 for i in range(n):
-    matchURL = "https://www.hltv.org/matches/" + str(matchIDsToFetch[i]) + "/*"
-    params.append({"cmd": "request.get", "url": matchURL, "maxTimeout": 60000})
+    # creating match URLs to fetch
+    matchURLToFetch = "https://www.hltv.org/matches/" + str(matchIDsToFetch[i]) + "/*"
+    params.append({"cmd": "request.get", "url": matchURLToFetch, "maxTimeout": 60000})
+
+    # using FlareSolverr to bypass Cloudflare for hltv.org
     response = requests.post(FlareSolverrURL, headers = FlareSolverrHeaders, json = params[i])
 
+    # parsing HTML
+    # searching HTML for all <a></a> tags
     html = BeautifulSoup(response.content, "html.parser")
     htmlLinks = html.find_all("a")
 
+    # search all found <a></a> tags for download IDs
+    # form download URLs from download IDs
     for j in htmlLinks:
         if "data-demo-link" in str(j):
-            print(str(j)[44:64])
+            print(str(i + 1) + "/" + str(n) + ",", str(j)[44:64] + ",", matchURLToFetch)
             downloadURLs.append("https://www.hltv.org" + str(j)[44:64])
 
+    # random delay to avoid bot detection
     sleep(rand(20, 30))
+
+# dictionary matching match IDs to download URLs
+downloadURLsJson = {matchIDsToFetch[i]:downloadURLs[i] for i in range(n)}
+
+# save this dictionary to downloadURLs.json
+with open("downloadURLs.json", "w") as download:
+    json.dump(downloadURLsJson, download)
+
+
+
+
+
