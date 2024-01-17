24204aa3ae63a05986e2493220141e97b2fa6692
Kevin Jia <kevinhengkaijia@gmail.com>
2024-01-17 00:04:06 +0000

    webbrowser instead of flaresolverr

--- src/demoDownloader.py
+++ src/demoDownloader.py
@@ -3,9 +3,11 @@ import pandas
 import numpy as np
 import math
 import sys
+import os
 import requests
 import urllib.request as url
 import multiprocessing
+import webbrowser
 
 from pathlib import Path
 from bs4 import BeautifulSoup
@@ -16,36 +18,44 @@ from time import sleep as sleep
 
 threads = 4
 
+webbrowser.open("", new = 0)
+
 # define FlareSolverr variables for bypassing Cloudflare
 FlareSolverrURL = "http://localhost:8191/v1"
 FlareSolverrHeaders = {"Content-Type": "application/json"}
 
 # match download function
-def download(downloadURL, matchID):
-    filePath = "/../demos/" + str(matchID)
+def download(matchID, downloadURL):
 
-    Path(fileLocation).mkdir(parents = True, exist_ok = True)
+    home = os.path.expanduser("~")
+    filePath = "../demos/" + str(matchID)
 
-    # Cloudflare once again poses an issue; this time for downloading matches themselves
-    # FlareSolverr is much harder to implement here
-    # it's probably possible, but frankly i can't be bothered
-    # a simple solution of opening the match download URL in the default browser is implemented here
-
+    Path(filePath).mkdir(parents = True, exist_ok = True)
 
     params = {"cmd": "request.get", "url": downloadURL, "maxTimeout": 60000}
 
-    requests.get(downloadURL, verify=False,stream=True)
+    # using FlareSolverr to bypass Cloudflare for hltv.org
+    response = requests.post(FlareSolverrURL, headers = FlareSolverrHeaders, json = params)
+    webbrowser.open(downloadURL, new = 2)
+
+    os.chdir(home + "/Downloads")
+
+    for
+
+    with open(str(matchID) + ".rar", "wb") as archive:
+        archive.write(response.content)
 
     # random delay to avoid bot detection
     sleep(rand(20, 30))
 
 # open matches.json for parsing
-with open("downloadURLs.json", "r") as events:
-    data = json.load(events)
+with open("downloadURLs.json", "r") as downloads:
+    data = json.load(downloads)
 
 # populate lists
-matchIDs = data.keys()
-downloadURLs = data.values()
-matches = np.column_stack((matchIDs, matchURLs))
+matchIDs = list(data.keys())
+downloadURLs = list(data.values())
+matches = np.column_stack((matchIDs, downloadURLs))
 
-Path("/../demos/" + str(matchIDs))
+for i in range(1):
+    download(matches[i][0], matches[i][1])
